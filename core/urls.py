c0cef42f642295cb43a4bf2e2761b14215864e4f
vinnieoh <viniciusoliveira926@gmail.com>
2022-05-04 13:04:51 -0300

    route correcao

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import MovieSearchAPIView, MovieListAPIView, MovieDestroyAPIView 
 
 urlpatterns = [
-    path('movie-search', MovieSearchAPIView.as_view(), name='movie-search'),
-    path('movie-list', MovieListAPIView.as_view(), name='movie-list'),
-    path('movie-destroy', MovieDestroyAPIView.as_view(), name='movie-destroy')
+    path('movie-search/', MovieSearchAPIView.as_view(), name='movie-search'),
+    path('movie-list/', MovieListAPIView.as_view(), name='movie-list'),
+    path('<int:pk>/movie-destroy', MovieDestroyAPIView.as_view(), name='movie-destroy')
 ]
